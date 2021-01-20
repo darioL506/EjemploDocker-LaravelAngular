82b461abec58f5f771f83c752b4404ae453f8fda
dario <darioleong1997@gmail.com>
2021-01-20 18:01:36 +0100

    first commit

---
+++ desarrollo/back/my-cool-app/database/migrations/2021_01_20_123024_create_bands_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBandsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bands', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('name');
+            $table->string('country');
+            $table->string('genre');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bands');
+    }
+}
